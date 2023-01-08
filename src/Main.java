9ef9d9e563119da3ee88f17632e3d52aa1aa2b36
Mykhailo Vasylenko <mykhailo.development@gmail.com>
2023-01-08 15:12:59 +0200

    test commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
-import queue.ArrayQueue;
-import queue.ListQueue;
+import queue.NodeQueue;
 import queue.Queue;
+import stack.NodeBasedStack;
+import stack.Stack;
 
 public class Main {
     public static void main(String[] args) {
 
-        Queue<Integer> custom = new ArrayQueue<>(3);
-        System.out.println(custom);
+        Queue<Integer> custom = new NodeQueue<>();
 
         custom.enqueue(1);
         custom.enqueue(2);
@@ -22,12 +22,9 @@ public class Main {
 
         System.out.println(custom);
 
-        custom.enqueue(4);
-
-        System.out.println(custom);
-
         custom.dequeue();
 
         System.out.println(custom);
+
     }
 }
