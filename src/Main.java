e3450c3857b48a34b63cdb9396be1ba76488f6ef
Mykhailo Vasylenko <mykhailo.development@gmail.com>
2023-01-11 20:34:11 +0200

    clean up main

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,7 @@
-import queue.NodeQueue;
-import queue.Queue;
-import stack.NodeBasedStack;
-import stack.Stack;
 
 public class Main {
+
     public static void main(String[] args) {
-
-        Queue<Integer> custom = new NodeQueue<>();
-
-        custom.enqueue(1);
-        custom.enqueue(2);
-        custom.enqueue(3);
-
-        System.out.println(custom);
-
-        custom.dequeue();
-
-        System.out.println(custom);
-
-        custom.dequeue();
-
-        System.out.println(custom);
-
-        custom.dequeue();
-
-        System.out.println(custom);
 
     }
 }
