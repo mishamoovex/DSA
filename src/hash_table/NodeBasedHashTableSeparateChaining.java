1f583fdabe288182f21a223ad04fbf01eeda3671
Mykhailo Vasylenko <mykhailo.development@gmail.com>
2023-01-20 00:26:26 +0200

    NodeBasedHashTableSeparateChaining refactoring

--- src/hash_table/NodeBasedHashTableSeparateChaining.java
+++ src/hash_table/NodeBasedHashTableSeparateChaining.java
@@ -5,13 +5,13 @@ import java.util.Iterator;
 @SuppressWarnings("unchecked")
 public class NodeBasedHashTableSeparateChaining<K, V> implements Hashable<K, V>, Iterable<V> {
 
-    private static class EntryNode<K, V> {
+    private static class Entry<K, V> {
         final int hash;
         K key;
         V value;
-        EntryNode<K, V> next;
+        Entry<K, V> next;
 
-        public EntryNode(K key, V value, EntryNode<K, V> next) {
+        public Entry(K key, V value, Entry<K, V> next) {
             this.key = key;
             this.value = value;
             this.next = next;
@@ -25,7 +25,7 @@ public class NodeBasedHashTableSeparateChaining<K, V> implements Hashable<K, V>,
 
         //We are not overriding the Object equals method
         //no casting is required with this method
-        public boolean equals(EntryNode<K, V> other) {
+        public boolean equals(Entry<K, V> other) {
             if (hash != other.hash) return false;
             return key.equals(other.key);
         }
@@ -37,9 +37,9 @@ public class NodeBasedHashTableSeparateChaining<K, V> implements Hashable<K, V>,
     private final double maxLoadFactor;
     private int capacity;
     private int threshold;
-    private int size = 0;
+    private int count = 0;
 
-    private EntryNode<K, V>[] table;
+    private Entry<K, V>[] table;
 
 
     public NodeBasedHashTableSeparateChaining() {
@@ -62,17 +62,17 @@ public class NodeBasedHashTableSeparateChaining<K, V> implements Hashable<K, V>,
         this.maxLoadFactor = maxLoadFactor;
         this.capacity = Math.max(capacity, DEFAULT_CAPACITY);
         threshold = (int) (capacity * maxLoadFactor);
-        table = (EntryNode<K, V>[]) new EntryNode[capacity];
+        table = (Entry<K, V>[]) new Entry[capacity];
     }
 
     @Override
     public int size() {
-        return size;
+        return count;
     }
 
     @Override
     public boolean isEmpty() {
-        return size == 0;
+        return count == 0;
     }
 
     @Override
@@ -81,12 +81,12 @@ public class NodeBasedHashTableSeparateChaining<K, V> implements Hashable<K, V>,
             clearBucket(table[i]);
             table[i] = null;
         }
-        size = 0;
+        count = 0;
     }
 
-    private void clearBucket(EntryNode<K, V> head) {
+    private void clearBucket(Entry<K, V> head) {
         while (head != null) {
-            EntryNode<K, V> next = head.next;
+            Entry<K, V> next = head.next;
             head.next = null;
             head.value = null;
             head = next;
@@ -96,108 +96,85 @@ public class NodeBasedHashTableSeparateChaining<K, V> implements Hashable<K, V>,
     @Override
     public boolean containsKey(K key) {
         checkKeyNotNull(key);
-        return bucketSeekEntry(key) != null;
+        int hash = key.hashCode();
+        int index = normalizeIndex(hash);
+        for (Entry<K, V> e = table[index]; e != null; e = e.next) {
+            if (e.key.equals(key)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public boolean containsValue(V value) {
         checkValueNotNull(value);
-
-        for (EntryNode<K, V> bucket : table) {
-            EntryNode<K, V> trav = bucket;
-            while (trav != null) {
-                if (trav.value.equals(value)) {
+        for (Entry<K, V> bucket : table) {
+            for (Entry<K, V> e = bucket; e != null; e = e.next) {
+                if (e.value.equals(value)) {
                     return true;
                 }
-                trav = trav.next;
             }
         }
-
         return false;
-    }
-
-
-    @Override
-    public V put(K key, V value) {
-        //Validate if there no nullable input data
-        checkKeyNotNull(key);
-        checkValueNotNull(value);
-        //Generates a table index by the hash code from the given key
-        int bucketIndex = normalizeIndex(key.hashCode());
-        //Gets the bucket by the hash index from the table
-        //If there is no value with the given key or any bucket that produces the same
-        //hash code (hash collision) -> returns null
-        EntryNode<K, V> bucket = table[bucketIndex];
-        //Search for an entry by the given key inside the bucket
-        if (bucket != null) {
-            //Traverse through the bucket until the node with the given key will be found
-            EntryNode<K, V> trav = bucket;
-            //We need to keep track the tail position to be able to add a new node
-            //if the item by the given key wasn't found in the bucket
-            //Tail position should be before traversal node
-            EntryNode<K, V> tail = null;
-            while (trav != null) {
-                //Update the tail position
-                tail = trav;
-                //If an entry by the given key was found -> update value and return removed one
-                if (trav.key.equals(key)) {
-                    V removedValue = trav.value;
-                    trav.value = value;
-                    return removedValue;
-                }
-                trav = trav.next;
-            }
-            //Add a new node at the end of the bucket list
-            tail.next = new EntryNode<>(key, value, null);
-        }
-        //Otherwise create a new bucket with the given key and value
-        else {
-            table[bucketIndex] = new EntryNode<>(key, value, null);
-        }
-        //Increase the size of the table and resize it if the data size reached the threshold
-        if (++size >= threshold) resizeTable(table.length * 2);
-        //Returns null if a new item has been added to the table
-        return null;
-    }
-
-    private void resizeTable(int capacity) {
-        //Update the capacity and the threshold
-        this.capacity = capacity;
-        threshold = (int) (maxLoadFactor * capacity);
-        //Create a table with updated capacity
-        EntryNode<K, V>[] newTable = (EntryNode<K, V>[]) new EntryNode[capacity];
-        //Iterate through each bucket in the old table and copy it
-        for (int i = 0; i < table.length; i++) {
-            //Check if the entry by the given index exists in the original table
-            EntryNode<K, V> entry = table[i];
-            //If the entry exists we need to copy it to the newTable
-            while (entry != null) {
-                //Calculates the hash to get a bucked from the newTable if its exists
-                int hashIndex = normalizeIndex(entry.hash);
-                EntryNode<K, V> bucket = newTable[hashIndex];
-                //If there is no bucket with by the given hash -> set it from the old table
-                if (bucket == null) {
-                    newTable[hashIndex] = entry;
-                } else {
-                    //Otherwise iterate through the bucket and add it at the tail
-                    while (bucket.next != null) {
-                        bucket = bucket.next;
-                    }
-                    bucket.next = entry;
-                }
-                //Move to the next entry from the old table bucket
-                entry = entry.next;
-            }
-            //To avoid memory leaks clear the data from the old bucked
-            //and remove it form the old table
-            table[i] = null;
-        }
-        table = newTable;
     }
 
     @Override
     public V get(K key) {
+        checkKeyNotNull(key);
+        int hash = key.hashCode();
+        int index = normalizeIndex(hash);
+        for (Entry<K, V> e = table[index]; e != null; e = e.next) {
+            if (e.hash == hash && e.value.equals(key)) {
+                return e.value;
+            }
+        }
         return null;
+    }
+
+    @Override
+    public V put(K key, V value) {
+        checkValueNotNull(value);
+        checkKeyNotNull(key);
+        int hash = key.hashCode();
+        int index = normalizeIndex(hash);
+        for (Entry<K, V> e = table[index]; e != null; e = e.next) {
+            if (e.hash == hash && e.key.equals(key)) {
+                V old = e.value;
+                e.value = value;
+                return old;
+            }
+        }
+
+        addEntry(hash, index, key, value);
+        return null;
+    }
+
+    private void addEntry(int hash, int index, K key, V value) {
+        if (count >= threshold) {
+            resizeTable(capacity * 2);
+            index = normalizeIndex(hash);
+        }
+        Entry<K, V> e = table[index];
+        table[index] = new Entry<>(key, value, e);
+        count++;
+    }
+
+    private void resizeTable(int capacity) {
+        this.capacity = capacity;
+        threshold = (int) (maxLoadFactor * capacity);
+
+        Entry<K, V>[] newTable = (Entry<K, V>[]) new Entry[capacity];
+
+        for (int i = 0; i < table.length; i++) {
+            for (Entry<K, V> e = table[i]; e != null; e = e.next) {
+                int hashIndex = normalizeIndex(e.hash);
+                e.next = newTable[hashIndex];
+                newTable[hashIndex] = e;
+            }
+            table[i] = null;
+        }
+        table = newTable;
     }
 
     @Override
@@ -216,7 +193,7 @@ public class NodeBasedHashTableSeparateChaining<K, V> implements Hashable<K, V>,
         StringBuilder sb = new StringBuilder();
         sb.append("{");
         for (int i = 0; i < capacity; i++) {
-            EntryNode<K, V> bucket = table[i];
+            Entry<K, V> bucket = table[i];
             if (bucket != null) {
                 sb.append(toString(bucket)).append(", ");
             }
@@ -225,9 +202,9 @@ public class NodeBasedHashTableSeparateChaining<K, V> implements Hashable<K, V>,
         return sb.toString();
     }
 
-    private String toString(EntryNode<K, V> bucket) {
+    private String toString(Entry<K, V> bucket) {
         StringBuilder sb = new StringBuilder().append("[");
-        EntryNode<K, V> trav = bucket;
+        Entry<K, V> trav = bucket;
         while (trav != null) {
             sb.append(trav);
             trav = trav.next;
@@ -251,23 +228,8 @@ public class NodeBasedHashTableSeparateChaining<K, V> implements Hashable<K, V>,
      * will always be positive.
      */
     private int normalizeIndex(int hashCode) {
-       // return (hashCode & 0x7FFFFFFF) % capacity;
+//         return (hashCode & 0x7FFFFFFF) % capacity;
         return (hashCode % 2) == 0 ? 1 : 2;
-    }
-
-    private EntryNode<K, V> bucketSeekEntry(K key) {
-        int tableIndex = normalizeIndex(key.hashCode());
-        EntryNode<K, V> bucket = table[tableIndex];
-        if (bucket == null) return null;
-
-        while (bucket != null) {
-            if (bucket.key.equals(key)) {
-                return bucket;
-            }
-            bucket = bucket.next;
-        }
-
-        return null;
     }
 
     private void checkKeyNotNull(K key) {
@@ -281,7 +243,7 @@ public class NodeBasedHashTableSeparateChaining<K, V> implements Hashable<K, V>,
     public static void main(String[] args) {
         NodeBasedHashTableSeparateChaining<Integer, Integer> custom = new NodeBasedHashTableSeparateChaining<>(4);
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 20; i++) {
             custom.put(i, i);
         }
 
